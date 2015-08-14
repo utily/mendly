3e92f8a9e461a0b7991b9fc488d956ea8b052be0
Simon Mika <simon.mika@imint.se>
2015-08-14 15:34:59 +0200

    First part of lexer.

--- Utilities/BufferedIterator.ts
+++ Utilities/BufferedIterator.ts
@@ -27,14 +27,14 @@ module U10sil.Utilities {
 		_buffer: T[]
 		constructor(private _backend: Iterator<T>) {
 		}
-		peek(position: number = 0): IteratorResult<T> {
-			var next: IteratorResult<T> = { done: true }
-			while (position > this._buffer.length - 1 && !(next = this._backend.next()).done) {
-				this._buffer.push(next.value)
+		peek(position: number = 0): T {
+			var next: T = null
+			while (position > this._buffer.length - 1 && (next = this._backend.next())) {
+				this._buffer.push(next)
 			}
-			return position > this._buffer.length - 1 ? { done: true } : { done: false, value: this._buffer[position] }
+			return position > this._buffer.length - 1 ? null : this._buffer[position]
 		}
-		next(): IteratorResult<T> {
+		next(): T {
 			var result = this.peek(0)
 			if (this._buffer.length > 1) {
 				this._buffer.slice()
--- Utilities/Iterator.ts
+++ Utilities/Iterator.ts
@@ -21,13 +21,7 @@
 // SOFTWARE.
 
 module U10sil.Utilities {
-	export interface IteratorResult<T> {
-		done: boolean;
-		value?: T;
-	}
 	export interface Iterator<T> {
-		next(value?: any): IteratorResult<T>;
-		return?(value?: any): IteratorResult<T>;
-		throw?(e?: any): IteratorResult<T>;
+		next(): T;
 	}
 }
