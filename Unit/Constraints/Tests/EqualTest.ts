aeec6042936007fc36fc21e3369c3201d1dd0ebb
Thomas Fanell <thomas.fanell@imint.se>
2015-09-04 14:47:25 +0200

    removed some useless tests

--- Unit/Constraints/Tests/EqualTest.ts
+++ Unit/Constraints/Tests/EqualTest.ts
@@ -22,6 +22,7 @@
 
 /// <reference path="../../Fixture" />
 /// <reference path="../../Constraints/Is" />
+/// <reference path="../../Constraints/NotModifier" />
 
 module U10sil.Unit.Tests {
 	import Is = Constraints.Is
@@ -34,29 +35,14 @@ module U10sil.Unit.Tests {
 			this.add("false is false", () => {
 				this.expect(false, Is.Equal().To(false))
 			})
-			this.add("true is not false", () => {
-				this.expect(true === false, Is.Equal().To(false))
-			})
-			this.add("false is not true", () => {
-				this.expect(false === true, Is.Equal().To(false))
-			})
-			this.add("foo equals foo", () => {
-				this.expect("foo" === "foo", Is.Equal().To(true))
-			})
-			this.add("foo does not equal bar", () => {
-				this.expect("foo" === "bar", Is.Equal().To(false))
-			})
-			this.add("null === null", () => {
+			this.add("null equals null", () => {
 				this.expect(null, Is.Equal().To(null))
 			})
-			this.add("undefined === undefined", () => {
+			this.add("undefined equals undefined", () => {
 				this.expect(undefined, Is.Equal().To(undefined))
 			})
-			this.add("\"5\" == 5", () => {
-				this.expect("5" == <any>5, Is.Equal().To(true))
-			})
-			this.add("\"5\" === 5 == false", () => {
-				this.expect("5" === <any>5, Is.Equal().To(false))
+			this.add("\"foo\" equals \"foo\"", () => {
+				this.expect("foo", Is.Equal().To("foo"))
 			})
 		}
 	}
