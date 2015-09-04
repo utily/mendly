a599418638ebfffc3fa355c7d91aec5b74cbd887
Thomas Fanell <thomas.fanell@imint.se>
2015-09-04 14:54:22 +0200

    renamed arguments in compare function

--- Unit/Constraints/CompareConstraint.ts
+++ Unit/Constraints/CompareConstraint.ts
@@ -24,7 +24,7 @@
 
 module U10sil.Unit.Constraints {
 	export class CompareConstraint extends Constraint {
-		constructor(private correct: any, private comparer: (objectA: any, objectB: any) => boolean, parent: Modifier = null) {
+		constructor(private correct: any, private comparer: (left: any, right: any) => boolean, parent: Modifier = null) {
 			super(parent)
 		}
 		test(value: any): boolean {
