simonmika/magic

Added error module.

source/Error/Handler.ts
@@ -1,0 +1,11 @@
+/// <reference path="Level" />
+/// <reference path="Type" />
+/// <reference path="Region" />
+/// <reference path="Message" />
+
+module Magic.Error {
+	export interface Handler {
+		raise(message: Message): void
+		raise(message: string, level?: Level, type?: Type, region?: Region): void
+	}
+}

source/Error/Message.ts
@@ -1,0 +1,10 @@
+/// <reference path="Type" />
+/// <reference path="Level" />
+
+module Magic.Error {
+	export class Message {
+		constructor(private type: Type, private level: Level, private message: Message, private location: Location) {
+
+		}
+	}
+}

source/Error/Position.ts
@@ -1,4 +1,4 @@
-module Magic.IO {
+module Magic.Error {
 	export class Position {
 		constructor(private line: number, private column: number) { }
 		getLine() { return this.line }

utily/mendly

Error/Handler.ts
@@ -20,15 +20,14 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-/// <reference path="../../typings/node/node" />
-/// <reference path="StringReader" />
+/// <reference path="Level" />
+/// <reference path="Type" />
+/// <reference path="Region" />
+/// <reference path="Message" />
 
-var fs = require("fs");
-
-module U10sil.IO {
-	export class FileReader extends StringReader {
-		constructor(path: string) {
-			super(fs.readFileSync(path, "utf-8"), path)
-		}
+module U10sil.Error {
+	export interface Handler {
+		raise(message: Message): void
+		raise(message: string, level?: Level, type?: Type, region?: Region): void
 	}
 }

Error/Message.ts
@@ -20,17 +20,13 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-/// <reference path="Position" />
-/// <reference path="Location" />
+/// <reference path="Type" />
+/// <reference path="Level" />
 
-module U10sil.IO {
-	export class Region {
-		constructor(private resource: string, private start: Position, private end: Position) { }
-		getResource() { return this.resource }
-		getStart() { return this.start }
-		getEnd() { return this.end }
-		toString() {
-			return this.resource + " @ " + this.start.toString() + " - " + this.end.toString()
+module U10sil.Error {
+	export class Message {
+		constructor(private type: Type, private level: Level, private message: Message, private location: Location) {
+
 		}
 	}
 }

Error/Position.ts
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-module U10sil.IO {
+module U10sil.Error {
 	export class Position {
 		constructor(private line: number, private column: number) { }
 		getLine() { return this.line }