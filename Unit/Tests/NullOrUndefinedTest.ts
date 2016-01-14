simonmika/magic

Added NullOrUndefined constraint and associated tests

source/Unit/Tests/NullOrUndefinedTest.ts
@@ -1,0 +1,29 @@
+/// <reference path="../Fixture" />
+/// <reference path="../Constraints/Is" />
+
+module Magic.Unit.Tests {
+	import Is = Constraints.Is
+	export class NullOrUndefinedTest extends Fixture {
+		constructor() {
+			super("NullOrUndefined")
+			this.add("null or undefined 1", () => {
+				this.expect(null, Is.NullOrUndefined())
+			})
+			this.add("null or undefined 2", () => {
+				this.expect(undefined, Is.NullOrUndefined())
+			})
+			this.add("null or undefined 3", () => {
+				var s: string = null
+				this.expect(s, Is.NullOrUndefined())
+			})
+			this.add("null or undefined 4", () => {
+				var s: string = undefined
+				this.expect(s, Is.NullOrUndefined())
+			})
+			this.add("null or undefined 5", () => {
+				var s: string = ""
+				this.expect(s, Is.Not().NullOrUndefined())
+			})
+		}
+	}
+}

utily/mendly

Unit/Tests/NullOrUndefinedTest.ts
@@ -20,29 +20,32 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-/// <reference path="Modifier" />
-/// <reference path="CompareConstraint" />
-/// <reference path="FalseConstraint" />
-/// <reference path="TrueConstraint" />
-/// <reference path="EqualModifier" />
-/// <reference path="NullConstraint" />
-/// <reference path="NullOrUndefinedConstraint" />
-/// <reference path="UndefinedConstraint" />
+/// <reference path="../Fixture" />
+/// <reference path="../Constraints/Is" />
 
-module U10sil.Unit.Constraints {
-	export class NotModifier extends Modifier {
-		constructor(parent: Modifier = null) {
-			super(parent)
+module U10sil.Unit.Tests {
+	import Is = Constraints.Is
+	export class NullOrUndefinedTest extends Fixture {
+		constructor() {
+			super("NullOrUndefined")
+			this.add("null or undefined 1", () => {
+				this.expect(null, Is.NullOrUndefined())
+			})
+			this.add("null or undefined 2", () => {
+				this.expect(undefined, Is.NullOrUndefined())
+			})
+			this.add("null or undefined 3", () => {
+				var s: string = null
+				this.expect(s, Is.NullOrUndefined())
+			})
+			this.add("null or undefined 4", () => {
+				var s: string = undefined
+				this.expect(s, Is.NullOrUndefined())
+			})
+			this.add("null or undefined 5", () => {
+				var s: string = ""
+				this.expect(s, Is.Not().NullOrUndefined())
+			})
 		}
-		test(value: any): boolean {
-			return !(this.testChild(value))
-		}
-		Null() { return new NullConstraint(this) }
-		NullOrUndefined() { return new NullOrUndefinedConstraint(this) }
-		Undefined() { return new UndefinedConstraint(this) }
-		False() { return new FalseConstraint(this) }
-		True() { return new TrueConstraint(this) }
-		Equal() { return new EqualModifier(this) }
-		Not() { return new NotModifier(this) }
 	}
 }