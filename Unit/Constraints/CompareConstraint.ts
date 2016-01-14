simonmika/magic

implemented basic test framework

source/Unit/Constraints/CompareConstraint.ts
@@ -1,0 +1,12 @@
+/// <reference path="Constraint" />
+
+module Magic.Unit.Constraints {
+	export class CompareConstraint extends Constraint {
+		constructor(private correct: any, private comparer: (objectA: any, objectB: any) => boolean, parent: Modifier = null) {
+			super(parent)
+		}
+		test(value: any): boolean {
+			return this.comparer(value, this.correct)
+		}
+	}
+}

source/Unit/Constraints/Is.ts
@@ -1,0 +1,15 @@
+/// <reference path="FalseConstraint" />
+/// <reference path="TrueConstraint" />
+/// <reference path="NullConstraint" />
+/// <reference path="EqualModifier" />
+/// <reference path="NotModifier" />
+
+module Magic.Unit.Constraints {
+	export class Is {
+		static True() { return new TrueConstraint() }
+		static False() { return new FalseConstraint() }
+		static Null() { return new NullConstraint() }
+		static Equal() { return new EqualModifier() }
+		static Not() { return new NotModifier() }
+	}
+}

source/Unit/Constraints/NotModifier.ts
@@ -1,0 +1,22 @@
+/// <reference path="Modifier" />
+/// <reference path="CompareConstraint" />
+/// <reference path="FalseConstraint" />
+/// <reference path="TrueConstraint" />
+/// <reference path="EqualModifier" />
+/// <reference path="NullConstraint" />
+
+module Magic.Unit.Constraints {
+	export class NotModifier extends Modifier {
+		constructor(parent: Modifier = null) {
+			super(parent)
+		}
+		test(value: any): boolean {
+			return !(this.testChild(value))
+		}
+		Null() { return new NullConstraint(this) }
+		False() { return new FalseConstraint(this) }
+		True() { return new TrueConstraint(this) }
+		Equal() { return new EqualModifier(this) }
+		Not() { return new NotModifier(this) }
+	}
+}

source/Unit/Tests/EqualTest.ts
@@ -1,0 +1,35 @@
+/// <reference path="../Fixture" />
+/// <reference path="../Constraints/Is" />
+
+module Magic.Unit.Tests {
+	import Is = Constraints.Is
+	export class EqualTest extends Fixture {
+		constructor() {
+			super("EqualTest")
+			this.add("true is true", () => {
+				this.expect(true, Is.Equal().To(true))
+			})
+			this.add("false is false", () => {
+				this.expect(false, Is.Equal().To(false))
+			})
+			this.add("true is not false", () => {
+				this.expect(true === false, Is.Equal().To(false))
+			})
+			this.add("false is not true", () => {
+				this.expect(false === true, Is.Equal().To(false))
+			})
+			this.add("foo equals foo", () => {
+				this.expect("foo" === "foo", Is.Equal().To(true))
+			})
+			this.add("foo does not equal bar", () => {
+				this.expect("foo" === "bar", Is.Equal().To(false))
+			})
+			this.add("null === null", () => {
+				this.expect(null, Is.Equal().To(null))
+			})
+			this.add("undefined === undefined", () => {
+				this.expect(undefined, Is.Equal().To(undefined))
+			})
+		}
+	}
+}

source/Unit/Tests/NotTest.ts
@@ -1,0 +1,23 @@
+/// <reference path="../Fixture" />
+/// <reference path="../Constraints/Is" />
+
+module Magic.Unit.Tests {
+	import Is = Constraints.Is
+	export class NotTest extends Fixture {
+		constructor() {
+			super("NotTest")
+			this.add("foobar is not null", () => {
+				this.expect("foobar", Is.Not().Null())
+			})
+			this.add("foobar is not equal to moobar", () => {
+				this.expect("foobar", Is.Not().Equal().To("moobar"))
+			})
+			this.add("foo === bar is not true", () => {
+				this.expect("foo" === "bar", Is.Not().True())
+			})
+			this.add("foo !== bar is not false", () => {
+				this.expect("foo" !== "bar", Is.Not().False())
+			})
+		}
+	}
+}

utily/mendly

Unit/Constraints/CompareConstraint.ts
@@ -20,42 +20,15 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-/// <reference path="Test" />
-/// <reference path="TestFailedError" />
-/// <reference path="./Constraints/Constraint" />
+/// <reference path="Constraint" />
 
-module U10sil.Unit {
-	export class Fixture {
-		private tests: Test[] = []
-		constructor(private name: string) {
+module U10sil.Unit.Constraints {
+	export class CompareConstraint extends Constraint {
+		constructor(private correct: any, private comparer: (objectA: any, objectB: any) => boolean, parent: Modifier = null) {
+			super(parent)
 		}
-		getName(): string { return this.name }
-		add(name: string, action: () => void): void {
-			this.tests.push(new Test(name, action))
-		}
-		run(): void {
-			var failures: TestFailedError[] = []
-			var success = true
-			this.tests.forEach(test => {
-				try {
-					test.run()
-				} catch (TestFailedError) {
-					success = false
-					TestFailedError.setTest(test)
-					failures.push(TestFailedError)
-				}
-			})
-			console.log(this.name + ":", success ? "passed" : "failed")
-			if(!success) {
-				failures.forEach(failure => {
-					console.log("  ->", failure.getTest().toString())
-				})
-				//process.exit(1)
-			}
-		}
-		expect(value: any, constraint: Constraints.Constraint): void {
-			if (!constraint.verify(value))
-				throw new TestFailedError(value, constraint)
+		test(value: any): boolean {
+			return this.comparer(value, this.correct)
 		}
 	}
 }

Unit/Constraints/Is.ts
@@ -20,42 +20,18 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-/// <reference path="Test" />
-/// <reference path="TestFailedError" />
-/// <reference path="./Constraints/Constraint" />
+/// <reference path="FalseConstraint" />
+/// <reference path="TrueConstraint" />
+/// <reference path="NullConstraint" />
+/// <reference path="EqualModifier" />
+/// <reference path="NotModifier" />
 
-module U10sil.Unit {
-	export class Fixture {
-		private tests: Test[] = []
-		constructor(private name: string) {
-		}
-		getName(): string { return this.name }
-		add(name: string, action: () => void): void {
-			this.tests.push(new Test(name, action))
-		}
-		run(): void {
-			var failures: TestFailedError[] = []
-			var success = true
-			this.tests.forEach(test => {
-				try {
-					test.run()
-				} catch (TestFailedError) {
-					success = false
-					TestFailedError.setTest(test)
-					failures.push(TestFailedError)
-				}
-			})
-			console.log(this.name + ":", success ? "passed" : "failed")
-			if(!success) {
-				failures.forEach(failure => {
-					console.log("  ->", failure.getTest().toString())
-				})
-				//process.exit(1)
-			}
-		}
-		expect(value: any, constraint: Constraints.Constraint): void {
-			if (!constraint.verify(value))
-				throw new TestFailedError(value, constraint)
-		}
+module U10sil.Unit.Constraints {
+	export class Is {
+		static True() { return new TrueConstraint() }
+		static False() { return new FalseConstraint() }
+		static Null() { return new NullConstraint() }
+		static Equal() { return new EqualModifier() }
+		static Not() { return new NotModifier() }
 	}
 }

Unit/Constraints/NotModifier.ts
@@ -20,42 +20,25 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-/// <reference path="Test" />
-/// <reference path="TestFailedError" />
-/// <reference path="./Constraints/Constraint" />
+/// <reference path="Modifier" />
+/// <reference path="CompareConstraint" />
+/// <reference path="FalseConstraint" />
+/// <reference path="TrueConstraint" />
+/// <reference path="EqualModifier" />
+/// <reference path="NullConstraint" />
 
-module U10sil.Unit {
-	export class Fixture {
-		private tests: Test[] = []
-		constructor(private name: string) {
+module U10sil.Unit.Constraints {
+	export class NotModifier extends Modifier {
+		constructor(parent: Modifier = null) {
+			super(parent)
 		}
-		getName(): string { return this.name }
-		add(name: string, action: () => void): void {
-			this.tests.push(new Test(name, action))
+		test(value: any): boolean {
+			return !(this.testChild(value))
 		}
-		run(): void {
-			var failures: TestFailedError[] = []
-			var success = true
-			this.tests.forEach(test => {
-				try {
-					test.run()
-				} catch (TestFailedError) {
-					success = false
-					TestFailedError.setTest(test)
-					failures.push(TestFailedError)
-				}
-			})
-			console.log(this.name + ":", success ? "passed" : "failed")
-			if(!success) {
-				failures.forEach(failure => {
-					console.log("  ->", failure.getTest().toString())
-				})
-				//process.exit(1)
-			}
-		}
-		expect(value: any, constraint: Constraints.Constraint): void {
-			if (!constraint.verify(value))
-				throw new TestFailedError(value, constraint)
-		}
+		Null() { return new NullConstraint(this) }
+		False() { return new FalseConstraint(this) }
+		True() { return new TrueConstraint(this) }
+		Equal() { return new EqualModifier(this) }
+		Not() { return new NotModifier(this) }
 	}
 }

Unit/Tests/EqualTest.ts
@@ -20,42 +20,38 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-/// <reference path="Test" />
-/// <reference path="TestFailedError" />
-/// <reference path="./Constraints/Constraint" />
+/// <reference path="../Fixture" />
+/// <reference path="../Constraints/Is" />
 
-module U10sil.Unit {
-	export class Fixture {
-		private tests: Test[] = []
-		constructor(private name: string) {
-		}
-		getName(): string { return this.name }
-		add(name: string, action: () => void): void {
-			this.tests.push(new Test(name, action))
-		}
-		run(): void {
-			var failures: TestFailedError[] = []
-			var success = true
-			this.tests.forEach(test => {
-				try {
-					test.run()
-				} catch (TestFailedError) {
-					success = false
-					TestFailedError.setTest(test)
-					failures.push(TestFailedError)
-				}
+module U10sil.Unit.Tests {
+	import Is = Constraints.Is
+	export class EqualTest extends Fixture {
+		constructor() {
+			super("EqualTest")
+			this.add("true is true", () => {
+				this.expect(true, Is.Equal().To(true))
 			})
-			console.log(this.name + ":", success ? "passed" : "failed")
-			if(!success) {
-				failures.forEach(failure => {
-					console.log("  ->", failure.getTest().toString())
-				})
-				//process.exit(1)
-			}
-		}
-		expect(value: any, constraint: Constraints.Constraint): void {
-			if (!constraint.verify(value))
-				throw new TestFailedError(value, constraint)
+			this.add("false is false", () => {
+				this.expect(false, Is.Equal().To(false))
+			})
+			this.add("true is not false", () => {
+				this.expect(true === false, Is.Equal().To(false))
+			})
+			this.add("false is not true", () => {
+				this.expect(false === true, Is.Equal().To(false))
+			})
+			this.add("foo equals foo", () => {
+				this.expect("foo" === "foo", Is.Equal().To(true))
+			})
+			this.add("foo does not equal bar", () => {
+				this.expect("foo" === "bar", Is.Equal().To(false))
+			})
+			this.add("null === null", () => {
+				this.expect(null, Is.Equal().To(null))
+			})
+			this.add("undefined === undefined", () => {
+				this.expect(undefined, Is.Equal().To(undefined))
+			})
 		}
 	}
 }

Unit/Tests/NotTest.ts
@@ -20,42 +20,26 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-/// <reference path="Test" />
-/// <reference path="TestFailedError" />
-/// <reference path="./Constraints/Constraint" />
+/// <reference path="../Fixture" />
+/// <reference path="../Constraints/Is" />
 
-module U10sil.Unit {
-	export class Fixture {
-		private tests: Test[] = []
-		constructor(private name: string) {
-		}
-		getName(): string { return this.name }
-		add(name: string, action: () => void): void {
-			this.tests.push(new Test(name, action))
-		}
-		run(): void {
-			var failures: TestFailedError[] = []
-			var success = true
-			this.tests.forEach(test => {
-				try {
-					test.run()
-				} catch (TestFailedError) {
-					success = false
-					TestFailedError.setTest(test)
-					failures.push(TestFailedError)
-				}
+module U10sil.Unit.Tests {
+	import Is = Constraints.Is
+	export class NotTest extends Fixture {
+		constructor() {
+			super("NotTest")
+			this.add("foobar is not null", () => {
+				this.expect("foobar", Is.Not().Null())
 			})
-			console.log(this.name + ":", success ? "passed" : "failed")
-			if(!success) {
-				failures.forEach(failure => {
-					console.log("  ->", failure.getTest().toString())
-				})
-				//process.exit(1)
-			}
-		}
-		expect(value: any, constraint: Constraints.Constraint): void {
-			if (!constraint.verify(value))
-				throw new TestFailedError(value, constraint)
+			this.add("foobar is not equal to moobar", () => {
+				this.expect("foobar", Is.Not().Equal().To("moobar"))
+			})
+			this.add("foo === bar is not true", () => {
+				this.expect("foo" === "bar", Is.Not().True())
+			})
+			this.add("foo !== bar is not false", () => {
+				this.expect("foo" !== "bar", Is.Not().False())
+			})
 		}
 	}
 }