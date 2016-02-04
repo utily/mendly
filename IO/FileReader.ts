b2d870f5ed1346e6c4c6feb304500866b20a0ef4
Simon Mika <simon.mika@imint.se>
2016-02-04 11:02:38 +0100

    Adapted to use as submodule.

--- IO/FileReader.ts
+++ IO/FileReader.ts
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-/// <reference path="../../typings/node/node" />
+/// <reference path="../../../typings/node/node" />
 /// <reference path="StringReader" />
 
 var fs = require("fs");
